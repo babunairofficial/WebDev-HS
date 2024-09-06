fc2b6c5897ef11ca6a03aa623f896a3265ed7cd1
Babu Nair <babunairofficial@gmail.com>
2024-09-06 18:42:44 +0530

    assignment qs

---
+++ week 1 - website/index.js
@@ -0,0 +1,20 @@
+//Assignment #1
+function sum(a,b){
+    return a+b;
+}
+
+let assgn1 = sum(3,7);
+console.log(assgn1);
+
+//Assignment #2
+function canVote(age){
+    if(age>18){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+let assgn2 = canVote(19);
+console.log(assgn2);
