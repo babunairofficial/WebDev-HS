945892c7aaa2706d48679422f08429310970fa4f
Babu Nair <babunairofficial@gmail.com>
2024-09-17 11:02:13 +0530

    JS functions - sum of numbers

--- week 2 - async JS/index.js
+++ week 2 - async JS/index.js
@@ -5,3 +5,15 @@ function sum(a, b){
 }
 
 console.log(sum(2,3));
+
+// find sum from 1 to a number
+
+function total(n){
+    let sumOfN = 0;
+    for(let i = 1; i <= n; i++){
+        sumOfN +=i;
+    }
+    return sumOfN
+}
+
+console.log(total(100));
