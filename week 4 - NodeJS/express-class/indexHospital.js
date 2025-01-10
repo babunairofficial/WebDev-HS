9778a216754376529b50fb03f876eeab2af07c25
Babu Nair <babunairofficial@gmail.com>
2025-01-10 22:15:27 +0530

    adding users

--- week 4 - NodeJS/express-class/indexHospital.js
+++ week 4 - NodeJS/express-class/indexHospital.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 
+const users = [{
+    name: "John",
+    kidneys: [{
+        healthy: false
+    }]
+}];
+
 app.get("/", function(req,res){
     
 })
