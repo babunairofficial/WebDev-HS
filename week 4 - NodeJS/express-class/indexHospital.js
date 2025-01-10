c1c621fb63fe841c4cadd1afd15746ef1d60c7a0
Babu Nair <babunairofficial@gmail.com>
2025-01-10 22:29:37 +0530

    GET endpoint

--- week 4 - NodeJS/express-class/indexHospital.js
+++ week 4 - NodeJS/express-class/indexHospital.js
@@ -9,7 +9,20 @@ const users = [{
 }];
 
 app.get("/", function(req,res){
-    
+    const johnKidneys = users[0].kidneys;
+    const numberOfKidneys = johnKidneys.length;
+    let numberOfHealthyKidneys = 0;
+    for(let i = 0; i<johnKidneys.length; i++){
+        if(johnKidneys[i].healthy){
+            numberOfHealthyKidneys = numberOfHealthyKidneys + 1;
+        }
+    }
+    const numberOfUnhealthyKidneys = numberOfKidneys - numberOfHealthyKidneys;
+    res.json({
+        numberOfKidneys,
+        numberOfHealthyKidneys,
+        numberOfUnhealthyKidneys
+    })
 })
 
 app.listen(3000);
