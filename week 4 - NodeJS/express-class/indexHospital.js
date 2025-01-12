c87ab1e15ce4bb875813aed46ce37f1345890891
Babu Nair <babunairofficial@gmail.com>
2025-01-12 12:29:29 +0530

    post endpoint

--- week 4 - NodeJS/express-class/indexHospital.js
+++ week 4 - NodeJS/express-class/indexHospital.js
@@ -6,7 +6,9 @@ const users = [{
     kidneys: [{
         healthy: false
     }]
-}];
+}]; //acts as a mini-database, where data is stored
+
+app.use(express.json()); // to parse json for now
 
 app.get("/", function(req,res){
     const johnKidneys = users[0].kidneys;
@@ -24,5 +26,16 @@ app.get("/", function(req,res){
         numberOfUnhealthyKidneys
     })
 })
+
+//post - sending a statement in the body
+app.post("/", function(req, res){
+    const isHealthy = req.body.isHealthy;
+    users[0].kidneys.push({
+        healthy: isHealthy
+    })
+    res.json({
+        msg: "Done!"
+    })
+}) 
 
 app.listen(3000);
