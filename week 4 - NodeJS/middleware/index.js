f6dbe46e7711827d91cdd12d42805bca3e3eb216
Babu Nair <babunairofficial@gmail.com>
2025-01-24 12:38:43 +0530

    using middlewares

--- week 4 - NodeJS/middleware/index.js
+++ week 4 - NodeJS/middleware/index.js
@@ -4,37 +4,25 @@ const express = require('express');
 
 const app = express();
 
-//function that returns a boolean if the age of the person is more than 14
-function isOldEnough(age){
+function isOldEnoughMiddleware(req, res, next){
+    const age = req.query.age; //taking the value of the age request
     if (age >= 14){
-        return true;
+        next();
     } else{
-        return false;
+        res.json({
+            msg: "Sorry you are not of age yet"
+        });
     }
 }
-app.get("/ride2", function(req, res){
-    if(isOldEnough(req.query.age)){
-        res.json({
-            msg: "You have successfully riden the ride 2"
-        });    
-    } else{
-        res.status(411).json({
-            msg: "Sorry you are not of age yet"
-        });
-    }
-    
+app.get("/ride2", isOldEnoughMiddleware, function(req, res){
+    res.json({
+        msg: "You have successfully riden the ride 2"
+    });     
 })
-app.get("/ride1", function(req, res){
-    if(isOldEnough(req.query.age)){
-        res.json({
-            msg: "You have successfully riden the ride 1"
-        });    
-    } else{
-        res.status(411).json({
-            msg: "Sorry you are not of age yet"
-        });
-    }
-    
-})
+app.get("/ride1", isOldEnoughMiddleware, function(req, res){
+    res.json({
+        msg: "You have successfully riden the ride 1"
+    });     
+});
 
 app.listen(3000);
