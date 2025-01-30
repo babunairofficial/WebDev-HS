d388d48b7b0ba1332c0ef31aeafbfc50e347b810
Babu Nair <babunairofficial@gmail.com>
2025-01-30 18:21:13 +0530

    fetch API - basics

---
+++ week 5 - HTTP Servers/fetchAPI/script.js
@@ -0,0 +1,9 @@
+//fetch API
+
+/*stage 1 - basics */
+//fetch method sends a request to the http server and gets a response
+//using a dummy API
+// fetch("https://jsonplaceholder.typicode.com/posts/1");
+
+//also can fetch from local json
+fetch("here.json"); 
