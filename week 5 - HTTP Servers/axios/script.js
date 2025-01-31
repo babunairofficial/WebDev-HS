a30ee609d8157ed13802c0958606e6e846e21abd
Babu Nair <babunairofficial@gmail.com>
2025-01-31 12:23:01 +0530

    axios external library

---
+++ week 5 - HTTP Servers/axios/script.js
@@ -0,0 +1,17 @@
+//axios
+
+//axios is a JavaScript library
+//It is promise-based and acts as a HTTP client for NodeJs and browser
+
+async function getRecentPost(){
+    const response = await axios.get("https://jsonplaceholder.typicode.com/posts/1");
+    //axios converts data to json by default.
+    //the request GET is mentioned.
+    //incases where the request is not mentioned default is GET
+    
+    console.log(response.data);
+    document.getElementById("posts").innerHTML = response.data.title;
+    //alternatively response.data can be stored in a variable if needed
+
+}
+getRecentPost();
