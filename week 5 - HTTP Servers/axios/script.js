31dfb65cbc97e09e2efb72664139af0b76ce0748
Babu Nair <babunairofficial@gmail.com>
2025-01-31 19:36:10 +0530

    axios - add posts onclick

--- week 5 - HTTP Servers/axios/script.js
+++ week 5 - HTTP Servers/axios/script.js
@@ -10,8 +10,17 @@ async function getRecentPost(){
     //incases where the request is not mentioned default is GET
     
     console.log(response.data);
+    
     document.getElementById("posts").innerHTML = response.data.title;
     //alternatively response.data can be stored in a variable if needed
-
 }
 getRecentPost();
+
+async function getRemainingPosts(){
+    //adding more posts using button onclick event.
+    const addresponse = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    const arr = addresponse.data;
+    for(let i = 0; i < arr.length; i++){
+        document.getElementById("posts").innerHTML += ("<div>" + arr[i].body + "</div>");
+    }
+}
