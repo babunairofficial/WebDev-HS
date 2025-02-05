2873c8c264046ddc89b57c7ecb182fe80dda5292
Babu Nair <babunairofficial@gmail.com>
2025-02-05 20:10:56 +0530

    middlewares - basics

--- week 5 - HTTP Servers/middlewares/script.js
+++ week 5 - HTTP Servers/middlewares/script.js
@@ -4,12 +4,23 @@ const app = express();
 //create a global variable to count the number of requests made
 let requestCount = 0;
 
-//let us consider two endpoints
-app.get("/add", function(req,res){ //endpoint 1 
+// instead of using the same logic to check at multiple places, we can use a function to perform the same task within the function
+// call for the function at each endpoints to check for the conditions
+
+function requestIncrement() {
     //increment the count of request everytime a request is made
     requestCount++;
     //display the requesCount made so far on the console.
     console.log("Total number of requests made = " + requestCount);
+
+    //the middleware does not have access to the request object
+}
+
+//let us consider two endpoints
+app.get("/add", function(req,res){ //endpoint 1 
+    
+    //call for the function to increase the request Count.
+    requestIncrement();
 
     // main logic
     const a = parseInt(req.query.a);
@@ -21,10 +32,9 @@ app.get("/add", function(req,res){ //endpoint 1
 });
 
 app.get("/multiply", function(req,res){ //endpoint 2
-    //increment the count of request everytime a request is made
-    requestCount++;
-    //display the requesCount made so far on the console.
-    console.log("Total number of requests made = " + requestCount);
+    
+    //call for the function to increase the request Count.
+    requestIncrement();
 
     // main logic
     const a = parseInt(req.query.a);
