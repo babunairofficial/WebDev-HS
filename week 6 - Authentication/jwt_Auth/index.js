bcc8b3d4aae05bb19de04f1cffd254afdca1c0be
Babu Nair <babunairofficial@gmail.com>
2025-02-28 19:19:04 +0530

    verify jwt

--- week 6 - Authentication/jwt_Auth/index.js
+++ week 6 - Authentication/jwt_Auth/index.js
@@ -16,3 +16,8 @@ const token = jwt.sign(value, "secret");
 //the token has been generated using a particular secret, hence the token can be verified only using this secret.
 
 console.log(token);
+
+//sign and not generate
+const verifiedValue = jwt.verify("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiYmFidW5haXIiLCJhY2NvdW50TnVtYmVyIjoxMjM0NTEyMzQ1LCJpYXQiOjE3NDA3NDI3MzF9.3mMw2SYXFrtiegqJIeSSxVIYqpYcH10C8cqbYU7qS7g", "secret");
+
+console.log(verifiedValue);
