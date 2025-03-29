a75c750e2220145fd7cf5be40f14259b62cce8a4
Babu Nair <babunairofficial@gmail.com>
2025-03-29 23:00:41 +0530

    course selling app - add add database endpoint

---
+++ week 8 - course selling app/backendOfCourseSellingApp_1/db.js
@@ -0,0 +1,45 @@
+const {Schema} = require("mongoose");
+mongoose.connect("");
+
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
+
+const userSchema = new Schema({
+    //_id would be generated automatically 
+    email: {type: String, unique: true},
+    passowrd: String,
+    firstName: String,
+    lastName: String,
+});
+
+const adminSchema = new Schema({
+    email: {type: String, unique: true},
+    passowrd: String,
+    firstName: String,
+    lastName: String,
+});
+
+const courseSchema = new Schema({
+    title: String,
+    description: String,
+    price: Number,
+    imageUrl: String,
+    creatorId: ObjectId
+});
+
+const purchaseSchema = new Schema({
+    userId: ObjectId,
+    courseId: ObjectId
+});
+
+const userModel = mongoose.model("user", userSchema);
+const adminModel = mongoose.model("admin", adminSchema);
+const courseModel = mongoose.model("course", courseSchema);
+const purchaseModel = mongoose.model("purchase", purchaseSchema);
+
+modeule.exports = {
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+}
