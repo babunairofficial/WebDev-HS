b3aed5cb9bfce7fd46d3cb003591073a8f20eca4
Babu Nair <babunairofficial@gmail.com>
2025-04-11 22:53:27 +0530

    course selling app - admin signup route functionalities

--- week 8 - course selling app/backendOfCourseSellingApp_1/routes/admin.js
+++ week 8 - course selling app/backendOfCourseSellingApp_1/routes/admin.js
@@ -2,7 +2,18 @@ const express = require('express');
 const adminRouter = express.Router();
 const {adminModel} = require("../db"); 
 
-adminRouter.post('/signup', (req, res) => {
+adminRouter.post('/signup', async (req, res) => {
+    const { email, password, firstName, lastName } = req.body; //add zod validation
+    
+    //hash the passowrd
+
+    await adminModel.create({
+        email: email,
+        password: password,
+        firstName: firstName,
+        lastName: lastName
+    });
+
     res.json({
         message: "signup endpoint"
     });
