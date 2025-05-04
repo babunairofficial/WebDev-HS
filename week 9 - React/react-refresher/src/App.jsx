6982e21bac73e656a7df90a5e898ee5a726a2542
Babu Nair <babunairofficial@gmail.com>
2025-05-04 23:00:10 +0530

    using props to understand conditional rendering

--- week 9 - React/react-refresher/src/App.jsx
+++ week 9 - React/react-refresher/src/App.jsx
@@ -7,15 +7,34 @@ function App() {
             <div style={{display: "flex", justifyContent: "center"}}>
                 <div>
                     <div>
-                        <PostComponent />
+                        <PostComponent 
+                            name={"sachin"}
+                            subtitle={"30 followers"}
+                            time={"3m ago"}
+                            image={"https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?t=st=1746336689~exp=1746340289~hmac=07154c1575f9ea226aa19eb8334fe2bb161de9399d8d7701e6dc12fafc590b88&w=826"}
+                            description={"How to get hired in 2025? Here are a list of challenges."}
+                        />
+                        
                         <br />
                     </div>
                     <div>
-                        <PostComponent />
+                    <PostComponent 
+                            name={"viru"}
+                            subtitle={"Promoted"}
+                            time={"9m ago"}
+                            image={"https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?t=st=1746336689~exp=1746340289~hmac=07154c1575f9ea226aa19eb8334fe2bb161de9399d8d7701e6dc12fafc590b88&w=826"}
+                            description={"Learn to be explosive"}
+                        />
                         <br />
                     </div>
                     <div>
-                        <PostComponent />
+                    <PostComponent 
+                            name={"rahul"}
+                            subtitle={20}
+                            time={"15m ago"}
+                            image={"https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?t=st=1746336689~exp=1746340289~hmac=07154c1575f9ea226aa19eb8334fe2bb161de9399d8d7701e6dc12fafc590b88&w=826"}
+                            description={"Master your emotions."}
+                        />
                         <br />
                     </div>
                 </div>
@@ -30,26 +49,30 @@ function App() {
 
 const style = { width: 200, backgroundColor: "white", borderRadius: 10, borderColor: "grey", borderWidth: 1, padding: 20};
 
-function PostComponent() {
+function PostComponent({name, subtitle, time, image, description}) {
 
     return <div style={style}>
         <div style={{display: "flex"}}>
-            <img src={"https://sdmntprwestus.oaiusercontent.com/files/00000000-ca44-6230-8e43-f4eb7245c9e9/raw?se=2025-05-02T17%3A56%3A04Z&sp=r&sv=2024-08-04&sr=b&scid=628a6bbc-ac2d-5703-a524-5b69dcc84e13&skoid=3f3a9132-9530-48ef-96b7-fee5a811733f&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-05-02T02%3A48%3A05Z&ske=2025-05-03T02%3A48%3A05Z&sks=b&skv=2024-08-04&sig=w54wRymPgrtwdQHpVOI5lHKV3/MIbq1amGZ6H3E2/F8%3D"}  style={{
-                width: 20,
-                height: 20,
+            <img src={image} style={{
+                width: 30,
+                height: 30,
                 borderRadius:20
             }} />
             <div style={{fontSize: 10, marginLeft: 10}}>
                 <b>
-                    100xdevs
+                    {name}
                 </b>
-                <div>26,748 followers</div>
-                <div>12m</div>
+                <div>{subtitle}</div>
+                <div style={{display: "flex"}}>
+                    <div>{time}</div>
+                    <img src={"https://w7.pngwing.com/pngs/473/569/png-transparent-graphy-clock-clock-icon-angle-number-noun-project-thumbnail.png"} style={{width: 10, height: 10}} />
+
+                </div>
             </div>        
             
         </div>
         <div style={{fontSize: 12}}>
-            What to know how to win big? Check out how these folks won $6000 in bounties.
+            {description}
         </div>
     </div>
 
