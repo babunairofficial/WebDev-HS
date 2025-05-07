6414b9da7c88170e1541b6b58bffd14fac8918de
Babu Nair <babunairofficial@gmail.com>
2025-05-07 23:53:05 +0530

    Post component through an array of objects

--- week 9 - React/react-refresher/src/App.jsx
+++ week 9 - React/react-refresher/src/App.jsx
@@ -4,7 +4,23 @@ import { PostComponent } from "./Post";
 
 function App() {
 
-    
+    const posts =  [{
+        name: "sachin",
+        subtitle: "1000 followers",
+        time: "2m ago",
+        image: "https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?t=st=1746336689~exp=1746340289~hmac=07154c1575f9ea226aa19eb8334fe2bb161de9399d8d7701e6dc12fafc590b88&w=826",
+        description: "trying to create a post component using a posts array"
+    }];
+
+    //[<PostComponent>]
+
+    const postComponents = posts.map(post => <PostComponent
+        name={post.name}
+        subtitle={post.subtitle}
+        time={post.title}
+        image={post.image}
+        description={post.description}
+        />)
 
     function addPost() {
 
@@ -16,13 +32,7 @@ function App() {
             <div style={{display: "flex", justifyContent: "center"}}>
                 
                 <div>
-                    <PostComponent 
-                    name={"sachin"}
-                    subtitle={"30 followers"}
-                    time={"3m ago"}
-                    image={"https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?t=st=1746336689~exp=1746340289~hmac=07154c1575f9ea226aa19eb8334fe2bb161de9399d8d7701e6dc12fafc590b88&w=826"}
-                    description={"How to get hired in 2025? Here are a list of challenges."}
-                    />
+                    {postComponents}
 
                 
                 </div>
