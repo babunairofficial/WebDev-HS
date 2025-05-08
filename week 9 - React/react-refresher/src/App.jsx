a121dfdc24c64ecdd3ad09b9e315b6b6eefac7f4
Babu Nair <babunairofficial@gmail.com>
2025-05-08 11:20:38 +0530

    push another set of post component in the array & re-render it

--- week 9 - React/react-refresher/src/App.jsx
+++ week 9 - React/react-refresher/src/App.jsx
@@ -1,16 +1,12 @@
 // import { useState } from "react";
+import { useState } from "react";
 import { PostComponent } from "./Post";
 
 
 function App() {
+    const [ posts, setPosts ] = useState([]);
 
-    const posts =  [{
-        name: "sachin",
-        subtitle: "1000 followers",
-        time: "2m ago",
-        image: "https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?t=st=1746336689~exp=1746340289~hmac=07154c1575f9ea226aa19eb8334fe2bb161de9399d8d7701e6dc12fafc590b88&w=826",
-        description: "trying to create a post component using a posts array"
-    }];
+    
 
     //[<PostComponent>]
 
@@ -23,7 +19,14 @@ function App() {
         />)
 
     function addPost() {
-
+        setPosts([...posts, {
+                name: "viru",
+                subtitle: "900 followers",
+                time: "20m ago",
+                image: "https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?t=st=1746336689~exp=1746340289~hmac=07154c1575f9ea226aa19eb8334fe2bb161de9399d8d7701e6dc12fafc590b88&w=826",
+                description: "pushing another set of post component"
+        }])
+        
     }
 
     return (
