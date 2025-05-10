412dccedc104a80c105d05d63071ae0eef06451d
Babu Nair <babunairofficial@gmail.com>
2025-05-10 21:02:28 +0530

    error boundary in react

--- week 9 - React/react-refresher-part2/src/App.jsx
+++ week 9 - React/react-refresher-part2/src/App.jsx
@@ -1,37 +1,63 @@
-import { useEffect, useState } from "react";
+import React from "react";
 
 
 function App() {
-  const [isComponentShown, setIsComponentShown] = useState(true);
-
-  return (
-    <div>
-      {isComponentShown && <MyComponent></MyComponent>}
-    </div>
-  )
-}
-
-function MyComponent() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    console.log('Component mounted or count updated');
-
-  }, [count]);
-
-  useEffect(() => {
-    console.log('Component mounted');
-    return() =>{
-      console.log('Compoent will unmount');
-    };
-  }, [])
   
+
   return (
     <div>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <ErrorBoundary>
+        <Card1></Card1>
+      </ErrorBoundary>
+      
+      <ErrorBoundary>
+        <Card2></Card2>  
+      </ErrorBoundary>
+      
     </div>
   )
 }
+
+function Card1() {
+//if error throws this card would crash
+  throw new Error("Error while rendering");
+  return <div style= {{background: "red", borderRadius: 20, padding: 20}}>
+    hi there
+  </div>
+}
+
+function Card2() {
+  return <div style={{background: "red", borderRadius: 20, padding: 20, margin: 20}}>
+    hello 
+  </div>
+}
+
+class ErrorBoundary extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error) {
+        return { hasError: true };
+    }
+
+    componentDidCatch(error, info) {
+        console.error("Error caught:", error, info);
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return <h1>Something went wrong.</h1>;
+        }
+
+        return this.props.children; 
+    }
+}
+
+const BuggyComponent = () => {
+    throw new Error("I crashed!");
+};
+
 
 export default App;
