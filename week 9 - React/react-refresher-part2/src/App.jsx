cca5d7fbbee56766a41087e3056faddef1ae6b07
Babu Nair <babunairofficial@gmail.com>
2025-05-10 20:41:41 +0530

    lifecycle events in react

--- week 9 - React/react-refresher-part2/src/App.jsx
+++ week 9 - React/react-refresher-part2/src/App.jsx
@@ -1,69 +1,37 @@
+import { useEffect, useState } from "react";
 
 
 function App() {
-  
-  const todos = [
-    {
-      id: 1,
-      title: "GO to gym",
-      done: false
-    }, 
-    {
-      id: 2,
-      title: "Eat Food",
-      done: true
-    }
-  ];
-  const todosComponents = todos.map(todo => <Todo key={todo.id} title={todo.title} done={todo.done}></Todo>) //key should be unique
-  
-  return <div style={{display: "flex", background: "grey"}}>
-    <Card >
-      <h2>Card Title</h2>
-      <p>There is some content inside the card.</p>
-      <div style={{color: "green"}}>
-        hello there 
-        <input type={"text"}></input>
-      </div>
-    </Card>
-    <Card>
-      <h2>Another Card</h2>
-      <textarea type="text"></textarea>
-    </Card>
-    <div>
-      {todosComponents}
+  const [isComponentShown, setIsComponentShown] = useState(true);
 
-      
-      {/* another approach to include Todo component, instead of using todosComponents */}
-
-      {[
-        <Todo key={3} title={"play cricket"} done={false}></Todo>
-      ]}  
-    </div>
-    
-    
-    
-  </div>
-}
-
-function Card({ children }) {
   return (
-    <div style={{
-      border: "1px solid #ccc",
-      background: "white", 
-      borderRadius: 10, 
-      padding: 20, 
-      margin: 10,
-      boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.1)"
-      }}>
-        {children}
+    <div>
+      {isComponentShown && <MyComponent></MyComponent>}
     </div>
-  );
+  )
 }
 
-function Todo({title, done}) {
-  return <div>
-    {title} - {done ? "Done!" : "Not done!"}
-  </div>
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log('Component mounted or count updated');
+
+  }, [count]);
+
+  useEffect(() => {
+    console.log('Component mounted');
+    return() =>{
+      console.log('Compoent will unmount');
+    };
+  }, [])
+  
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  )
 }
 
 export default App;
