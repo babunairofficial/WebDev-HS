fb4b91662e1f14d0336736b947794ffd3d941e56
Babu Nair <babunairofficial@gmail.com>
2025-05-11 23:09:20 +0530

    Links for Navigation in React

--- week 10 - React continued/React-part2/src/App.jsx
+++ week 10 - React continued/React-part2/src/App.jsx
@@ -1,11 +1,16 @@
 
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
 function App() {
   return <div>
-    MyCoachingClass | Class 11 | Class 12
+    
     <BrowserRouter>
+      <Link to="/">MyCoachingClass</Link> 
+      | 
+      <Link to="/neet/online-coaching-class-11">Class 11</Link>
+      | 
+      <Link to="/neet/online-coaching-class-12">Class 12</Link>
       <Routes>
         <Route path="/neet/online-coaching-class-11" element={<Class11Program />}></Route>
         <Route path="/neet/online-coaching-class-12" element={<Class12Program />}></Route>
