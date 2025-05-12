7345151e68ad27b5e2582b39a48e396bb9b82baf
Babu Nair <babunairofficial@gmail.com>
2025-05-12 16:54:33 +0530

    rolling up the state in React

---
+++ week 10 - React continued/React-part3/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return <div>
+    <LightBulb />
+  </div>
+}
+
+function LightBulb() {
+  const [bulbOn, setBulbOn] = useState(true);
+
+  return <div>
+    <BulbState bulbOn={bulbOn} />
+    <ToggleBulbState bulbOn={bulbOn} setBulbOn={setBulbOn} />
+  </div>
+}
+
+function BulbState({bulbOn}) {
+  
+
+  return <div>
+    {bulbOn ? "Bulb on" : "Bulb off"}
+  </div>
+}
+
+function ToggleBulbState({bulbOn, setBulbOn}) {
+
+  function toggle() {
+    setBulbOn(!bulbOn) //toggle between true and false
+    console.log(bulbOn)
+    
+  }
+
+  return <div>
+    <button onClick={toggle}> Toggle the bulb </button>
+  </div>
+}
+
+export default App
