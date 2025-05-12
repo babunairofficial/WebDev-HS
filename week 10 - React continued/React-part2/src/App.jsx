1bfd27f1292741e3d2de408d06dcb90df736f1f8
Babu Nair <babunairofficial@gmail.com>
2025-05-12 07:44:21 +0530

    layouts in React

--- week 10 - React continued/React-part2/src/App.jsx
+++ week 10 - React continued/React-part2/src/App.jsx
@@ -1,21 +1,19 @@
 
 import './App.css'
-import { BrowserRouter, Routes, Route, Link, useNavigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
 
 function App() {
   return <div>
     
     <BrowserRouter>
-      <Link to="/">MyCoachingClass</Link> 
-      | 
-      <Link to="/neet/online-coaching-class-11">Class 11</Link>
-      | 
-      <Link to="/neet/online-coaching-class-12">Class 12</Link>
+      
       <Routes>
-        <Route path="/neet/online-coaching-class-11" element={<Class11Program />}></Route>
-        <Route path="/neet/online-coaching-class-12" element={<Class12Program />}></Route>
-        <Route path="/" element={<Landing />} />
-        <Route path="*" element={<ErrorPage />}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route path="/neet/online-coaching-class-11" element={<Class11Program />}></Route>
+          <Route path="/neet/online-coaching-class-12" element={<Class12Program />}></Route>
+          <Route path="/" element={<Landing />} />
+          <Route path="*" element={<ErrorPage />}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   </div>
@@ -52,6 +50,23 @@ function Landing() {
 function ErrorPage() {
   return <div>
     Sorry page not found
+  </div>
+}
+
+function Layout() {
+  return <div style={{height: "100vh"}}>
+
+    <Link to="/">MyCoachingClass</Link> 
+      | 
+    <Link to="/neet/online-coaching-class-11">Class 11</Link>
+      | 
+    <Link to="/neet/online-coaching-class-12">Class 12</Link>
+    
+    <div style={{height: "90vh"}}>
+      <Outlet />
+    </div>
+    
+    footer
   </div>
 }
 export default App
