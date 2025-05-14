6c70ac1065d3818a6aa3a98e2a1db4844aa88d2e
Babu Nair <babunairofficial@gmail.com>
2025-05-14 23:50:11 +0530

    counter app using custom hooks

--- week 11 - React continued/custom-hooks/src/App.jsx
+++ week 11 - React continued/custom-hooks/src/App.jsx
@@ -1,11 +1,20 @@
 import { useState } from "react"
 
-function App() {
+// custom hook
+function useCounter() {
   const [count, setCount] = useState(0);
 
   function increaseCount() {
     setCount(count + 1)
   }
+  return {
+    count: count,
+    increaseCount: increaseCount
+  }
+}
+
+function App() {
+  const {count, increaseCount} = useCounter();
   return (
 
     <div>
