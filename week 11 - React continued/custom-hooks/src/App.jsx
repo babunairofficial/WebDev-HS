ba1b1539fc2dd4d377a1953bb5bd186b819a1b4a
Babu Nair <babunairofficial@gmail.com>
2025-05-15 21:46:23 +0530

    useFetch without hooks

--- week 11 - React continued/custom-hooks/src/App.jsx
+++ week 11 - React continued/custom-hooks/src/App.jsx
@@ -1,42 +1,25 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
-// custom hook
-function useCounter() {
-  const [count, setCount] = useState(0);
-
-  function increaseCount() {
-    setCount(count + 1)
-  }
-  return {
-    count: count,
-    increaseCount: increaseCount
-  }
-}
 
 function App() {
+  const [post, setPost] = useState({});
+
+  //define the function
+  async function getPosts() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+    const json = await response.json();
+    setPost(json);
+  }
+  useEffect(() => {
+    getPosts(); //called a function, which is defined above
+  }, [])
 
   return (
     <div>
-      <Counter></Counter>
-      <Counter></Counter>
-      <Counter></Counter>
-      <Counter></Counter>
+      {post.title}
     </div>
   )
 }
 
-//component
-function Counter() {
-  const {count, increaseCount} = useCounter();
-  return (
-
-    <div>
-      <button onClick={increaseCount}>Increase</button>
-      <br />
-      {count}
-    </div>
-    
-  )
-}
 
 export default App
