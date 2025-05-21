e5383258889bc0a682e8109a638f7cdf5afa59a9
Babu Nair <babunairofficial@gmail.com>
2025-05-21 21:13:15 +0530

    usePrev hook working chronology in React

--- week 11 - React continued/custom-hooks/src/hooks/use-prev.js
+++ week 11 - React continued/custom-hooks/src/hooks/use-prev.js
@@ -2,12 +2,15 @@ import { useEffect, useRef } from "react";
 
 export const usePrev = (value) => {
     const ref = useRef();
-    //useRef does not re-render anythin
+    //useRef does not re-render anything
+    console.log("re-render happened with new value " + value);
 
     useEffect(() => {
+        console.log("updated the ref to be " + value);
         ref.current = value;
     }, [value]);
 
-    return ref.current;
+    console.log("returned " + ref.current);
+    return ref.current; //undefined
 }
 // it returns first, effect get called later.
